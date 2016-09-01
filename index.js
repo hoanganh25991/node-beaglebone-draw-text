35a1a357842a72f09f4d9f8793ce6b2d3282d52c
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-09-01 23:49:10 +0700

    try to SELF run, rather than johnny-five serialport command

--- index.js
+++ index.js
@@ -1,38 +1,27 @@
-var five = require('johnny-five');
+var b = require('bonescript');
 
-var BeagleBone = require('./beaglebone-io');
-
-var board = new five.Board({
-    io: new BeagleBone()
-});
-// var drawText = require('./draw-text');
 var drawTxt = require('./draw-text-v2');
 
-board.on('ready', function() {
-	var matrix = new five.Led.Matrix({
-		pins: {
-			data: 'P9_14',
-			clock: 'P9_21',
-			cs: 'P9_16'
-		},
-		devices: 4
-	});
-	matrix.on();
+var Matrix = require('./led/matrix');
 
-	matrix.board = board;
-
-	matrix.MATRIX_CHARS = five.LedControl.MATRIX_CHARS;
-
-	matrix.drawText = drawTxt.bind(matrix);
-	
-	// matrix.drawText('1234');
-	// board.loop(1000, function(){
-	// 	matrix.io.digitalWrite('P9_14', matrix.io.HIGH);
-	// });
-	
-
-	this.repl.inject({
-		matrix: matrix
-	});
-
+var matrix = new Matrix({
+	pins: {
+		data: 'P9_14',
+		clock: 'P9_21',
+		cs: 'P9_16'
+	},
+	devices: 4,
+	controller: 'MAX 7219'
 });
+
+matrix.board = b;
+// matrix.on();
+
+// matrix.on();
+//
+// matrix.board = board;
+//
+//
+matrix.drawText = drawTxt.bind(matrix);
+
+matrix.drawText('abc');
